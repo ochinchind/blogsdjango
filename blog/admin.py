74b67fb79a0eca2895c26d795f7373cf51222075
ochinchind <amanrahim93@gmail.com>
2024-09-04 20:57:26 +0500

    step 4

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at')
+    search_fields = ('title',)
