478cc43259f29b2adbf6909e5a27cb21c14c0baa
ochinchind <amanrahim93@gmail.com>
2024-09-04 21:21:36 +0500

    step 6 7

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('hello/', views.hello_blog, name='hello_blog'),
     path('', views.post_list, name='post_list'),
     path('<int:pk>/', views.post_detail, name='post_detail'),
+    path('create/', views.post_create, name='post_create'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 
 from django.http import HttpResponse
 from .models import *
+from .forms import *
 
 def hello_blog(request):
     return HttpResponse("Hello, Blog!")
@@ -16,3 +17,13 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_form.html', {'form': form})
