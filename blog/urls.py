f78fa9010a4fc03f3b8446f2cc51dbcaadc431e0
ochinchind <amanrahim93@gmail.com>
2024-09-04 21:33:38 +0500

    step 8

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('<int:pk>/', views.post_detail, name='post_detail'),
     path('create/', views.post_create, name='post_create'),
+    path('<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('<int:pk>/delete/', views.post_delete, name='post_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseForbidden
 from .models import *
 from .forms import *
 
@@ -27,3 +27,34 @@ def post_create(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_form.html', {'form': form})
+
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    
+    if post.author != request.user:
+        return HttpResponseForbidden("You are not allowed to edit this post.")
+    
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    
+    return render(request, 'blog/post_form.html', {'form': form, 'is_edit': True})
+
+@login_required
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    
+    if post.author != request.user:
+        return HttpResponseForbidden("You are not allowed to delete this post.")
+    
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+    
+    return render(request, 'blog/post_confirm_delete.html', {'post': post})
